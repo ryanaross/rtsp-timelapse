8d2baf08fb743a37238d4da565fae97731a2f69e
Ryan Ross <ryanaross@gmail.com>
2023-11-02 18:28:28 -0700

    adding capability to make todays video

--- webserver.py
+++ webserver.py
@@ -1,10 +1,12 @@
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, Response
 import glob
 import os
 import logging
 import config
 from datetime import datetime
 import mimetypes
+import subprocess
+
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
 
@@ -12,9 +14,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def serve_status():
+    logging.debug('Serving status...')
     directories = ['stills', 'videos', 'timelapse']
     directories_info = []
     for directory in directories:
+        logging.debug(f'Processing directory: {directory}')
         files = glob.glob(f'{directory}/*')
         file_count = len(files)
         latest_file = max(files, key=os.path.getctime) if files else None
@@ -34,8 +38,34 @@ def serve_status():
     config_variables = {name: value for name, value in config.__dict__.items() if not name.startswith('__')}
     return render_template('status.html', directories=directories_info, latest_image=latest_image, config_variables=config_variables)
 
+
+@app.route('/make_video', methods=['POST'])
+def make_video():
+    logging.debug('Making video...')
+    # Run the make_video.py script
+    subprocess.run(["python", "make_video.py"], check=True)
+
+    # Get the most recent file in the videos directory
+    video_files = glob.glob('videos/*')
+    if not video_files:
+        return 'No video files found', 404
+
+    latest_video_file = max(video_files, key=os.path.getctime)
+
+    if os.path.exists(latest_video_file):
+        with open(latest_video_file, 'rb') as f:
+            data = f.read()
+        response = Response(data, mimetype='video/mp4')
+        response.headers['Content-Disposition'] = 'attachment; filename={}'.format(os.path.basename(latest_video_file))
+        os.remove(latest_video_file)  # Delete the file after sending it
+        logging.debug(f'Removing video at {latest_video_file}')
+        return response
+    else:
+        return 'Video not found', 404
+
 @app.route('/files/<path:directory>')
 def serve_file_list(directory):
+    logging.debug(f'Serving file list for directory: {directory}')
     # Check if the directory exists
     if os.path.exists(directory):
         # Get a list of files in the directory
